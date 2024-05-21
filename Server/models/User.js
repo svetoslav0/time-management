162de2c2553709368d3c9b8174bbfbae9dff99e5
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-21 20:38:42 +0300

    TMP-10 - Improve user schema with unique usernames, timestamps

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,24 +1,30 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 userSchema.pre("save", async function () {
     this.password = await bcrypt.hash(this.password, 12);
