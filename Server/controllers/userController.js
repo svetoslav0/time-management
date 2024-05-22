1506bbe89554142c78eace4f61a53313d884b262
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-05-22 11:06:57 +0300

    Added controller endpoint for user creation

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -12,3 +12,16 @@ router.post("/login", async (req, res) => {
         res.status(401).json({ message: "Invalid username or password" });
     }
 });
+
+router.post("/user", async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const user = await userService.createUser(userData);
+
+        res.status(200).json(user);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
