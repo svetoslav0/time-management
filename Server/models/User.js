8b37a030c4d09c74424bac0358805d5077b7518e
DaniIliev <danieljivkov04@abv.bg>
2024-05-22 11:27:44 +0300

    add ref

--- Server/models/User.js
+++ Server/models/User.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        userRole: {
+            type: String,
+            ref: 'Role'
+        }
     },
     {
         timestamps: true,
