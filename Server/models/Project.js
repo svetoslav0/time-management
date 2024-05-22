be04a139eb4304883b8296fb13f54027cbd3f9a5
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-05-22 12:29:48 +0300

    Create Project mongoose model

---
+++ Server/models/Project.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  clientName: {
+    type: String,
+    required: true
+  },
+  projectName: {
+    type: String,
+    required: true
+  },
+  startingDate: {
+    type: Date,
+    required: true
+  },
+  pricePerHour: {
+    type: Number,
+    required: true
+  },
+  employeeIds: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Employee'
+  }]
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+module.exports = Project;
