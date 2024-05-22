c3729de0aa36cfd2b4f7b427019637fb3d2fcf10
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-05-22 12:45:41 +0300

    Added comments for better readability

--- Server/services/userService.js
+++ Server/services/userService.js
@@ -26,38 +26,55 @@ exports.createUser = async (userData) => {
     const { username, firstName, lastName, password, userRole  } = userData;
     //TODO CORRECT THE VALIDATION FOR userRole.
     //TODO ADD VALIDATION FOR DIFFERENT KIND OF USERS
-    await validateUserData(username, firstName,firstName , password, userRole);
+
+    // Validate user data
+    await validateUserData(username, firstName, lastName, password, userRole);
 
     try {
-    const user = await User.create({ username: username, firstName: firstName, lastName:lastName, password: password, userRole:userRole });
+    // Create the user in the database
+    const user = await User.create({ 
+        username: username,
+        firstName: firstName,
+        lastName:lastName,
+        password: password,
+        userRole:userRole 
+        });
 
-    }
-    catch(error)
-    {
-        throw new Error("Invalid credentials!");
-    }
-
+    // Return the created user information
     return {
         username: user.username,
         firstName: user.firstName,
         lastName: user.lastName,
         userRole: user.userRole
+    };
+
     }
+    catch(error)
+    {
+        // Handle errors, e.g., if username is already taken
+        throw new Error("Error creating user: " + error.message);
+    };
+
 };
 
+// Function to validate user data before creating a new user
 const validateUserData = async (username, firstName, lastName, password, userRole) => {
 
+    // Check if the username is at least 2 characters long
     if (username.length < 2) {
         throw new Error('Username is not long enough');
     }
-
+    
+    // Check if the password is at least 6 characters long
     else if (password.length < 6) {
         throw new Error('Password is not long enough');
-    }
-
+    };
+    
+    // Check if a user with the same username already exists in the database
     const doesUserExist = await User.findOne({ username });
 
     if (doesUserExist) {
-        throw new Error('User exists');
-    }
+        // If user with the same username exists, throw an error
+        throw new Error('User exists!');
+    };
 };
