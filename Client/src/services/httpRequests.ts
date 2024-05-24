224a874eb612be58f4efa63a6bdbd0d244842502
AlexPetrov88 <112386979+AlexPetrov88@users.noreply.github.com>
2024-05-24 10:23:36 +0300

    Separate Concerns

---
+++ Client/src/services/httpRequests.ts
@@ -0,0 +1,51 @@
+import { RequestOptions } from '../shared/types';
+
+// const host = process.env.REACT_APP_API_HOST || 'http://localhost:5173';
+const host = 'http://localhost:5173';
+
+export async function httpRequest<T, V>(options: RequestOptions<T>): Promise<V> {
+    const { url, method, data, headers } = options;
+    const fetchHeaders = new Headers({
+        // Authorization: 'Bearer /* TOKEN */',
+        'Content-Type': 'application/json',
+        ...headers,
+    });
+
+    const request: RequestInit = {
+        method,
+        headers: fetchHeaders,
+    };
+
+    if (data) {
+        if (data instanceof FormData) {
+            request.body = data;
+            fetchHeaders.delete('Content-Type');
+        } else {
+            request.body = JSON.stringify(data);
+        }
+    }
+
+    try {
+        const response = await fetch(host + url, request);
+
+        if (!response.ok) {
+            if (response.status === 403) {
+                // clearUserData();
+            }
+            const error = await response.json();
+            throw new Error(error.message || `HTTP Request failed with status ${response.status}`);
+        }
+
+        if (response.status === 204) {
+            return {} as V;
+        }
+
+        return response.json();
+    } catch (error) {
+        if (error instanceof Error) {
+            throw error;
+        } else {
+            throw new Error('HTTP Request failed due to an unknown error');
+        }
+    }
+}
--- Client/src/shared/types.ts
+++ Client/src/shared/types.ts
@@ -11,3 +11,21 @@ export type CreateUserDataType = {
     password: string;
     rePassword?: string;
 };
+
+// requestsTypes
+
+export type MethodType = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
+export interface RequestOptions<T> {
+    url: string;
+    method: MethodType;
+    data?: T;
+    headers?: Record<string, string>;
+}
+
+export interface HttpService {
+    get<V>(url: string, headers?: Record<string, string>): Promise<V>;
+    post<T, V>(url: string, data: T, headers?: Record<string, string>): Promise<V>;
+    put<T, V>(url: string, data: T, headers?: Record<string, string>): Promise<V>;
+    delete<V>(url: string, headers?: Record<string, string>): Promise<V>;
+}
