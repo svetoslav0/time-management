dd5b48f48ceb2a3e3fd3603f62873a275de149f9
DaniIliev <danieljivkov04@abv.bg>
2024-05-29 12:52:42 +0300

    add router.use('/project', projectController)

--- Server/routes.js
+++ Server/routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
 const userController = require("./controllers/userController");
+const projectController = require("./controllers/projectController")
 
 router.use("/users", userController);
-
+router.use("/project", projectController)
 module.exports = router;
