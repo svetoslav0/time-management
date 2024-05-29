21aca937f1f5c540617f5a8f4cd4fed4fc151a18
TimiPashov <pashov91@gmail.com>
2024-05-29 14:22:29 +0300

    TMP-27: Login makes request to server and save the response user in localStorage,  changed host variable to http://localhost:3000 from http://localhost:5173, install and configure CORS on the Server with origin http://localhost:5173, no credentials:true yet.

--- Client/src/components/auth/Login.tsx
+++ Client/src/components/auth/Login.tsx
@@ -1,6 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { SubmitHandler,useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
+import httpServices from '../../services/httpServices';
 import { loginSchema } from '../../shared/formValidations';
 import { LoginFormDataType } from '../../shared/types';
 import InputComponent from '../../UI/formComponents/InputComponent';
@@ -16,8 +17,11 @@ export default function Login() {
         resolver: yupResolver(loginSchema),
     });
 
-    const onSubmit: SubmitHandler<LoginFormDataType> = (data) => {
-        console.log(data);
+    const onSubmit: SubmitHandler<LoginFormDataType> = async (data) => {
+        const response = await httpServices().post('/users/login', data);
+        if (response) {
+            localStorage.setItem('user', JSON.stringify(response)); // Convert response to a string
+        }
         reset();
     };
 
@@ -25,11 +29,22 @@ export default function Login() {
         <div className='mx-auto flex max-w-sm flex-col gap-6 p-5'>
             <h2 className='self-center'>Login</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <InputComponent error={errors.username?.message} register={register} trigger={trigger} field='username' />
-                <InputComponent error={errors.password?.message} register={register} trigger={trigger} field='password' type='password' />
+                <InputComponent
+                    error={errors.username?.message}
+                    register={register}
+                    trigger={trigger}
+                    field='username'
+                />
+                <InputComponent
+                    error={errors.password?.message}
+                    register={register}
+                    trigger={trigger}
+                    field='password'
+                    type='password'
+                />
                 <button
                     type='submit'
-                    className='w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-full dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                    className='w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-full'
                 >
                     Login
                 </button>
