c8c76211e62bd4bb35bf43ea428a94712f647538
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2024-05-30 13:32:35 +0300

    TMP-19: removed comments

--- Server/controllers/projectController.js
+++ Server/controllers/projectController.js
@@ -1,21 +1,16 @@
 const router = require('express').Router()
 const projectService = require('../services/projectService')
 
-// Route to handle POST requests to create a new project
 router.post("/", async (req, res) => {
     
-    // Extract project data from the request body
     const projectData = req.body;
 
     try {
-        // Call the createProject function from the projectService to create a new project
         const project = await projectService.createProject(projectData);
 
-        // If project creation is successful, send a success response with the created project
         res.status(201).json(project);
     }
     catch (error) {
-        // If an error occurs during project creation, send a failure response with the error message
         res.status(403).json({ message: error.message });
     }
 });
