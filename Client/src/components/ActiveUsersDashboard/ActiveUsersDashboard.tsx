a1d86a85822f66923a5e013cf2c83f549cd05d98
TimiPashov <pashov91@gmail.com>
2024-05-30 20:20:30 +0300

    TMP-37: created global type User, UserCard component; type login response with type User

--- Client/src/components/ActiveUsersDashboard/ActiveUsersDashboard.tsx
+++ Client/src/components/ActiveUsersDashboard/ActiveUsersDashboard.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useState } from 'react';
 
-// import httpServices from '../../services/httpServices';
+import { User } from '../../shared/types';
+import UserCard from './UserCard';
 
-type User = {
-    username: string;
-    firstName: string;
-    lastName: string;
-    role: string;
-    active: boolean;
-};
+// import httpServices from '../../services/httpServices';
 
 export default function ActiveUsersDashboard() {
     const [activeUsers, setActiveUsers] = useState<User[]>([]);
@@ -16,18 +11,31 @@ export default function ActiveUsersDashboard() {
 
     useEffect(() => {
         const users: User[] = [
-            { username: 'Johnny', firstName: 'John', lastName: 'Doe', role: 'admin', active: true },
             {
-                username: 'Jane',
+                username: 'JohnnySinsJr',
+                firstName: 'John',
+                lastName: 'Doe',
+                role: 'admin',
+                active: true,
+            },
+            {
+                username: 'Jan365',
                 firstName: 'Jane',
                 lastName: 'Doe',
                 role: 'employee',
                 active: true,
             },
             {
-                username: 'Peter',
+                username: 'Petar4o',
                 firstName: 'Peter',
                 lastName: 'Petrov',
+                role: 'customer',
+                active: true,
+            },
+            {
+                username: 'WolverineX',
+                firstName: 'Hugh',
+                lastName: 'Jackman',
                 role: 'customer',
                 active: true,
             },
@@ -43,41 +51,16 @@ export default function ActiveUsersDashboard() {
     }, []);
 
     return (
-        <div className='mx-auto flex max-w-3xl flex-col gap-6 p-5'>
+        <div className='mx-auto flex flex-col gap-6 p-5'>
             <h2 className='self-center'>Active Users</h2>
             {loading ? (
                 <div className='self-center'>Loading...</div>
             ) : (
-                <table className='w-full table-fixed'>
-                    <thead>
-                        <tr className='bg-gray-300 dark:bg-gray-700'>
-                            <th className='w-1/4 p-2'>Username</th>
-                            <th className='w-1/4 p-2'>First Name</th>
-                            <th className='w-1/4 p-2'>Last Name</th>
-                            <th className='w-1/4 p-2'>Role</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {activeUsers.map((user) => (
-                            <tr
-                                key={user.username}
-                                className=' cursor-pointer border-b-2
-                                 border-gray-300
-                                  bg-gray-100 transition-colors
-                                   duration-300 ease-in-out even:bg-gray-200
-                                    hover:bg-white dark:border-gray-700 dark:bg-gray-800'
-                                onClick={() => {
-                                    console.log(user.username);
-                                }}
-                            >
-                                <td className='p-2 pl-14'>{user.username}</td>
-                                <td className='p-2 pl-14'>{user.firstName}</td>
-                                <td className='p-2 pl-14'>{user.lastName}</td>
-                                <td className='p-2 pl-14'>{user.role}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <div className='flex flex-wrap gap-4'>
+                    {activeUsers.map((user) => (
+                        <UserCard key={user.username} user={user} />
+                    ))}
+                </div>
             )}
         </div>
     );
