2fc36c4dd5f7590ebc92a5e683d0bec03985a2af
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-31 15:34:08 +0300

    TMP-48 - Create send cookie with token to the client logic

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -2,19 +2,18 @@ const router = require("express").Router();
 
 const userService = require("../services/userService");
 
-
 router.post("/login", async (req, res) => {
     const userData = req.body;
 
-
     try {
-        const user = await userService.login(userData);
+        const { user, token } = await userService.login(userData);
+
+        res.cookie("authCookie", token, { httpOnly: true, secure: true });
         res.status(200).json(user);
     } catch (error) {
         res.status(401).json({ message: "Invalid username or password" });
     }
 });
-
 
 // Route to handle POST requests to create a new user
 router.post("/user", async (req, res) => {
@@ -25,12 +24,12 @@ router.post("/user", async (req, res) => {
         // TODO: ADD ADDITIONAL VALIDATION FOR DIFFERENT TYPES OF USERS WHEN NEEDED
 
         // Call the createUser function from the userService to create a new user
-        const user = await userService.createUser(userData);
+        const { user, token } = await userService.createUser(userData);
 
         // If user creation is successful, send a success response with the created user
+        res.cookie("authCookie", token, { httpOnly: true, secure: true });
         res.status(200).json(user);
-    }
-    catch (error) {
+    } catch (error) {
         // If an error occurs during user creation, send a failure response with the error message
         res.status(400).json({ message: error.message });
     }
