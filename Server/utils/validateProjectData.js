7f2864021160ab336fd53fafb9d56663fcd5e39e
DaniIliev <danieljivkov04@abv.bg>
2024-06-01 12:10:37 +0300

    adding more descriptive error messages for cases of bad user input

--- Server/utils/validateProjectData.js
+++ Server/utils/validateProjectData.js
@@ -1,16 +1,36 @@
 const moment = require('moment');
+const User = require("../models/User");
 
-const validateProjectData = async (clientName, projectName, startingDate, pricePerHour, employeedIds) => {
-    if (clientName.length < 2) {
-        throw new Error('Username is not long enough');
+const validateProjectData = async (clientName, projectName, startingDate, pricePerHour, employeeIds) => {
+    if(!clientName){
+        throw new Error('Client Name is missing');
+    }
+    else if (clientName.length < 2) {
+        throw new Error('Client Name is not long enough');
+    }else if(!projectName){
+        throw new Error('Project Name is missing');
     }else if (projectName.length < 2) {
-        throw new Error('Project name is not long enough');
+        throw new Error('Project Name is not long enough');
+    }else if(!startingDate){
+        throw new Error('Starting Date is missing');
+    }else if(!pricePerHour){
+        throw new Error('Price per hour is missing');
+    }else if(!Number(pricePerHour)){
+        throw new Error('Price per hour has non-numeric value')
     }else if(Number(pricePerHour) <= 0){
-        throw new Error('The pay for one working hour is too low');
+        throw new Error('Price per hour has a negative numeric value');
+    }
+    else if(!employeeIds){
+        throw new Error('employeeIds is missing')
+    }
+    const users = await User.find({ _id: { $in: employeeIds } });
+
+    if (users.length !== employeeIds.length) {
+        throw new Error('employeeIds contains ID for user that does not exist')
     }
     const isValidDate = isValidDateMoment(startingDate)
     if(!isValidDate){
-        throw new Error('Invalid date!');
+        throw new Error('Starting Date is in incorrect format, it must be DD-MM-YYYY');
     }
 }
 
