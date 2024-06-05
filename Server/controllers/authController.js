730c751175ad586b7afcfc826675d2a93cf81843
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-05 17:29:32 +0300

    TMP-51 - Move user creation POST to authController and add isAdmin middleware

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -2,16 +2,34 @@ const router = require("express").Router();
 
 const userService = require("../services/userService");
 
+const isAdmin = require("../middlewares/isAdminMiddleware");
+
 router.post("/login", async (req, res) => {
     const userData = req.body;
 
     try {
         const { user, token } = await userService.login(userData);
 
-        res.cookie("authCookie", token, { httpOnly: true, secure: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
+        res.cookie("authCookie", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
         res.status(200).json(user);
     } catch (error) {
         res.status(401).json({ message: "Invalid username or password" });
+    }
+});
+
+router.post("/user", isAdmin, async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const user = await userService.createUser(userData);
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
 });
 
