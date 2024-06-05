8eaf38f0e6f113adfcb2c43c30f12d12e3028453
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-06-05 09:38:32 +0100

    TMP-54: add project form schema

--- Client/src/shared/formValidations.ts
+++ Client/src/shared/formValidations.ts
@@ -13,3 +13,19 @@ export const createUserSchema = yup.object().shape({
     password: yup.string().required('Password is required!'),
     rePassword: yup.string().oneOf([yup.ref('password'), undefined], 'Passwords must match!'),
 });
+
+export const projectFormSchema = yup.object().shape({
+    projectName: yup.string().required('Project name is required!'),
+    pricePerHour: yup
+        .number()
+        .required('Price per hour is required!')
+        .positive('Price must be bigger then 0')
+        .typeError('Price per hour must be a number'),
+    employeeIds: yup
+        .array()
+        .min(1, 'At least one employee must be selected')
+        .of(yup.string().required())
+        .required('At least one employee must be selected'),
+    customerId: yup.string().required('Select Customer!'),
+    startingDate: yup.string().required('Please select date'),
+});
