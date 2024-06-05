b7a1fb8a6df17d283a87222ba5be7556eb1de4e9
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-06-05 06:11:08 +0100

    TMP-54: add temporary types

---
+++ Client/src/components/projectForm/types.ts
@@ -0,0 +1,6 @@
+// EmployeesProps and ClientProps are temporary 
+export type EmployeesProps = Record<'id' | 'userName' | 'firstName' | 'lastName', string>;
+export type ClientProps = Record<
+    'id' | 'userName' | 'firstName' | 'lastName' | 'companyName',
+    string
+>;
