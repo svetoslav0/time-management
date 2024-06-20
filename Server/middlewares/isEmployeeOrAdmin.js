369e436f403aea58360214b0aba3242d02e915ee
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-20 16:57:08 +0300

    TMP-114 - Implement middleware to authorize employees and admins

---
+++ Server/middlewares/isEmployeeOrAdmin.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const userService = require("../services/userService");
+
+const isEmployeeOrAdmin = async (req, res, next) => {
+    const token = req.cookies.authCookie;
+
+    if (!token) {
+        return res.status(401).json({ message: "No token provided!" });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userService.getSingleUser(decodedToken._id);
+
+        if (
+            !user ||
+            (user.userRole !== "employee" && user.userRole !== "admin")
+        ) {
+            return res.status(401).json({ message: "unauthorized" });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid token!" });
+    }
+};
+
+module.exports = isEmployeeOrAdmin;
