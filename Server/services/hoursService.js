e0c0372d193b12a6af3a7a3f0951b548004e0dc1
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-20 21:43:19 +0300

    TMP-119 - Implement getAllHours function to retrieve all entries

--- Server/services/hoursService.js
+++ Server/services/hoursService.js
@@ -1,23 +1,26 @@
 const Hours = require("../models/Hours");
 
-const { validateHourDataOnLogHours } = require("../utils/validateHoursDataUtil");
+const {
+    validateHourDataOnLogHours,
+} = require("../utils/validateHoursDataUtil");
+
+exports.getAllHours = () => Hours.find();
 
 exports.logHours = async (hoursData) => {
     await validateHourDataOnLogHours(hoursData);
 
     const { projectId, userId, date, hours, notes } = hoursData;
-    
+
     try {
         await Hours.create({
             projectId,
             userId,
             date,
             hours,
-            notes
+            notes,
         });
-
     } catch (error) {
-        console.error('Error creating new Hours entity:', error);
+        console.error("Error creating new Hours entity:", error);
         throw new Error("Trouble logging hours!");
     }
 };
