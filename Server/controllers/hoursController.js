297c743b6ea325f8344c027a9e66b15c2987fb39
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-20 21:42:45 +0300

    TMP-119 - Add route to fetch all hours entries

--- Server/controllers/hoursController.js
+++ Server/controllers/hoursController.js
@@ -4,7 +4,6 @@ const hoursService = require("../services/hoursService");
 const isEmployeeOrAdmin = require("../middlewares/isEmployeeOrAdmin");
 const getJwtToken = require("../middlewares/getUserTokenMiddleware");
 
-
 router.post("/", isEmployeeOrAdmin, getJwtToken, async (req, res) => {
     const userId = req.userToken._id;
 
@@ -18,7 +17,15 @@ router.post("/", isEmployeeOrAdmin, getJwtToken, async (req, res) => {
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
+});
 
+router.get("/", async (req, res) => {
+    try {
+        const hours = await hoursService.getAllHours();
+        res.status(200).json(hours);
+    } catch (error) {
+        res.status(403).json({ message: error.message });
+    }
 });
 
 module.exports = router;
