c94cf8ffb6b81d421a288de176c8fa2f1e34d7b5
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-06-20 19:53:15 +0100

    TMP-102: Navigate to home page onSuccess

--- Client/src/components/auth/hooks/useCreateUser.ts
+++ Client/src/components/auth/hooks/useCreateUser.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 import { urlKeys } from '@/reactQuery/constants';
 import httpServices from '@/services/httpServices';
@@ -7,11 +8,13 @@ import { CreateUserDataType, User } from '@/shared/types';
 
 export default function useCreateUser() {
     const { post } = httpServices();
+    const navigate = useNavigate();
 
     const { mutate: createUser, isSuccess } = useMutation<User, Error, CreateUserDataType>({
         mutationFn: (data) => post<CreateUserDataType, User>(urlKeys.createUser, data),
         onSuccess: () => {
             toast.success('User created');
+            navigate('/')
         },
         onError: (error) => {
             toast.error(error.message);
