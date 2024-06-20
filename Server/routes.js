ec5e2578760b9b6b6bb7e3ac7f158bb59a68189d
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-06-20 16:59:53 +0300

    Added route for hours

--- Server/routes.js
+++ Server/routes.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 const authController = require("./controllers/authController");
 const userController = require("./controllers/userController");
 const projectController = require("./controllers/projectController");
+const hoursController = require("./controllers/hoursController");
 
 router.use("/", authController);
+router.use("/hours", hoursController);
 router.use("/users", userController);
 router.use("/projects", projectController);
 module.exports = router;
