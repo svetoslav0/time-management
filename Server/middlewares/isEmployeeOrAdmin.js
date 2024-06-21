87be5dc775437816b58fc9c9877fe87f8b2b9dc9
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-06-21 03:45:04 +0300

    Added role property for easier authentication

--- Server/middlewares/isEmployeeOrAdmin.js
+++ Server/middlewares/isEmployeeOrAdmin.js
@@ -20,6 +20,8 @@ const isEmployeeOrAdmin = async (req, res, next) => {
             return res.status(401).json({ message: "unauthorized" });
         }
 
+        user.userRole === "admin" ? req.isAdmin = true : req.isAdmin = false;
+
         next();
     } catch (error) {
         return res.status(403).json({ message: "Invalid token!" });
