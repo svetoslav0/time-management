74bafc3c9731069743eec66984a1ad2cb6b26843
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-06-22 16:29:02 +0100

    use ProjectResponseDataType type

--- Client/src/components/projectForm/hooks/useProjectCreate.ts
+++ Client/src/components/projectForm/hooks/useProjectCreate.ts
@@ -2,18 +2,17 @@ import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-import { ProjectFormDataType } from '../ProjectFormControl';
-
 import { urlKeys } from '@/reactQuery/constants';
 import httpServices from '@/services/httpServices';
+import { ProjectDataType, ProjectResponseDataType } from '@/shared/types';
 
 export default function useProjectCreate() {
     const { post } = httpServices();
     const navigate = useNavigate();
 
     const { mutate: createProject, isSuccess } = useMutation({
-        mutationFn: (data: ProjectFormDataType) =>
-            post<ProjectFormDataType, ProjectFormDataType>(urlKeys.createProject, data),
+        mutationFn: (data: ProjectDataType) =>
+            post<ProjectDataType, ProjectResponseDataType>(urlKeys.createProject, data),
         onSuccess: () => {
             toast.success('Project created');
             navigate('/');
