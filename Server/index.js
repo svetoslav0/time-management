36238b2a66afc4d685c4283c23eafb3df1c78fa9
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-24 22:06:42 +0300

    TMP-129 - Change console.error error message

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(routes);
 app.use((err, req, res, next) => {
-    console.error("Error:", err.message);
+    console.error("Error appeared in server:", err);
 
     if (err instanceof userValidationErrors) {
         return res.status(err.statusCode).json({ message: err.message });
