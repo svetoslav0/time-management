7f5a021ccb6c9fe348aa3e4ec705ae43b45252ce
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-24 22:00:37 +0300

    TMP-129 - Implement general error handling middleware

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const initializeAdmin = require("./utils/initializeAdmin");
+const userValidationErrors = require("./errors/userValidationErrors");
 
 const routes = require("./routes");
 const cookieParser = require("cookie-parser");
@@ -12,25 +13,34 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
 );
 
 app.use(routes);
+app.use((err, req, res, next) => {
+    console.error("Error:", err.message);
+
+    if (err instanceof userValidationErrors) {
+        return res.status(err.statusCode).json({ message: err.message });
+    }
+
+    res.status(500).json({ message: "Internal server error!" });
+});
 
 const mongoURI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(mongoURI)
-  .then(() => {
-    initializeAdmin();
-    console.log("MongoDb Connected!");
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+    .connect(mongoURI)
+    .then(() => {
+        initializeAdmin();
+        console.log("MongoDb Connected!");
+    })
+    .catch((err) => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5173;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
