3eaf19ae6ee904e629814244ceee3d52071cab1c
TimiPashov <pashov91@gmail.com>
2024-06-25 20:14:08 +0300

    Add isLoading to ProjectAdminDashboard

--- Client/src/components/project/ProjectAdminDashboard.tsx
+++ Client/src/components/project/ProjectAdminDashboard.tsx
@@ -3,16 +3,20 @@ import ProjectCard from './ProjectCard';
 import useFetchAllProjects from '@/reactQuery/hooks/useFetchAllProjects';
 
 export default function ProjectAdminDashboard() {
-    const { data: projects } = useFetchAllProjects();
+    const { data: projects, isLoading } = useFetchAllProjects();
 
     return (
         <>
             <h2 className='text-center'>Projects</h2>
-            <div className='mx-10 mt-4 grid gap-10 pb-10 md:grid-cols-2 lg:grid-cols-4'>
-                {projects?.map((project) => (
-                    <ProjectCard key={project.projectName} project={project} />
-                ))}
-            </div>
+            {isLoading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div className='mx-10 mt-4 grid gap-10 pb-10 md:grid-cols-2 lg:grid-cols-4'>
+                    {projects?.map((project) => (
+                        <ProjectCard key={project.projectName} project={project} />
+                    ))}
+                </div>
+            )}
         </>
     );
 }
