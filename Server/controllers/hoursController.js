120a7234ddcff9fe27db070934e5e5a75e1065ce
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-25 12:29:45 +0300

    TMP-129 - Refactor hours controller to send error to general errors middleware

--- Server/controllers/hoursController.js
+++ Server/controllers/hoursController.js
@@ -4,7 +4,9 @@ const hoursService = require("../services/hoursService");
 const isEmployeeOrAdmin = require("../middlewares/isEmployeeOrAdmin");
 const getJwtToken = require("../middlewares/getUserTokenMiddleware");
 
-router.post("/", isEmployeeOrAdmin, getJwtToken, async (req, res) => {
+const HoursValidationErrors = require("../errors/hoursValidationErrors");
+
+router.post("/", isEmployeeOrAdmin, getJwtToken, async (req, res, next) => {
     const userId = req.userToken._id;
 
     req.body.userId = userId;
@@ -12,19 +14,26 @@ router.post("/", isEmployeeOrAdmin, getJwtToken, async (req, res) => {
 
     try {
         const hours = await hoursService.logHours(hoursData);
-
+        if (!hours) {
+            throw new HoursValidationErrors("Hours not logged", 400);
+        }
         res.status(200).json(hours);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        next(error);
     }
 });
 
 router.get("/", async (req, res) => {
     try {
         const hours = await hoursService.getAllHours();
+
+        if (!hours) {
+            throw new HoursValidationErrors("Hours not found", 404);
+        }
+
         res.status(200).json(hours);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        next(error);
     }
 });
 
