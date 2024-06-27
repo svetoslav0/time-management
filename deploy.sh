4b46558c157efdd59799cd406c2ba1d67106f844
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2024-06-27 17:34:29 +0300

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
-#!/bin/bash
-
-export PORT=3000
-export MONGODB_URI=mongodb://localhost:27017/time-management
-export JWT_SECRET=aNpUdKbd4REvtmZuH1XDnZ5XVnDPwOsr
+echo "Deleting error logs"
+true > /var/lib/jenkins/.pm2/logs/api-error.log
 
 cd Server
 ls -sail
 
+echo "Running npm install . . ."
+npm install
+npm install dotenv
+
+pm2 ls
+
+echo "Restarting API service . . ."
 pm2 restart api
+# pm2 start index.js --name api
+
+sleep 10
+
+pm2 ls
+pm2 show api
+cat /var/lib/jenkins/.pm2/logs/api-error.log
