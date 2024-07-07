ac623b10aa5b7c9133b41d1c3747664181a5b867
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-07-07 18:24:23 +0100

    TMP-88: Create dashboard component to render employee or customer projects

---
+++ Client/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useLoginData } from '../auth/AuthContext';
+import EmployeeProjects from './employeeProjects/EmployeeProjects';
+
+export default function Dashboard() {
+    const { loginResponseData } = useLoginData();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!loginResponseData) {
+            navigate('/auth/login');
+        }
+    }, [loginResponseData, navigate]);
+
+    return (
+        <>
+            {loginResponseData?.userRole === 'customer' && <p>Customer projects</p>}
+            {(loginResponseData?.userRole === 'admin' ||
+                loginResponseData?.userRole === 'employee') && <EmployeeProjects />}
+        </>
+    );
+}
