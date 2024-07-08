cecc6f82983691a674ced7492252ea954dafb64b
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-07-08 14:18:21 +0100

    TMP-168: Create filter and select Fn

--- Client/src/reactQuery/hooks/useFetchAllProjects.ts
+++ Client/src/reactQuery/hooks/useFetchAllProjects.ts
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import { useCallback, useState } from 'react';
 
 import { queryKeys, urlKeys } from '../constants';
 
@@ -8,15 +9,31 @@ import { ProjectResponseDataType } from '@/shared/types';
 const { get } = httpServices();
 
 export default function useFetchAllProjects() {
+    const [filter, setFilter] = useState('');
+
+    const handleChangeFilter = useCallback((data: string) => {
+        setFilter(data);
+    }, []);
+
+    const selectFn = useCallback((data: ProjectResponseDataType[], filter: string) => {
+        if (filter) {
+            return data.filter((project) =>
+                project.projectName.toLowerCase().includes(filter.toLowerCase())
+            );
+        }
+        return data;
+    }, []);
+
     const queryKey = [queryKeys.projects];
     const generatedUrl = `${urlKeys.projects}`;
 
-    const { data, error, isLoading, refetch } = useQuery<ProjectResponseDataType[]>({
+    const { data, error, isLoading, refetch } = useQuery({
         queryKey,
         queryFn: () => get<ProjectResponseDataType[]>(generatedUrl),
+        select: (data) => selectFn(data, filter),
         staleTime: 1000 * 60 * 5,
         gcTime: 1000 * 60 * 10,
         retry: 2,
     });
-    return { data, error, isLoading, refetch };
+    return { data, error, isLoading, refetch, filter, handleChangeFilter };
 }
