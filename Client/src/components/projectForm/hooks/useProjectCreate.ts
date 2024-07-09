4b64755ba630b0b839417f7e00ac11076ec62a49
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-07-09 18:03:46 +0100

    TMP-174: Invalidate projects on success

--- Client/src/components/projectForm/hooks/useProjectCreate.ts
+++ Client/src/components/projectForm/hooks/useProjectCreate.ts
@@ -2,7 +2,8 @@ import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-import { urlKeys } from '@/reactQuery/constants';
+import { queryKeys, urlKeys } from '@/reactQuery/constants';
+import { queryClient } from '@/reactQuery/queryClient';
 import httpServices from '@/services/httpServices';
 import { ProjectDataType, ProjectResponseDataType } from '@/shared/types';
 
@@ -15,6 +16,9 @@ export default function useProjectCreate() {
             post<ProjectDataType, ProjectResponseDataType>(urlKeys.createProject, data),
         onSuccess: (data) => {
             toast.success(`${data.projectName} created successfully`);
+            queryClient.invalidateQueries({
+                queryKey: [queryKeys.projects],
+            });
             navigate('/');
         },
         onError: (error) => {
