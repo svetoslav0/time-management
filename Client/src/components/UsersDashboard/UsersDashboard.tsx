7f97e57a47877e88ecafb7fb80621e5cc4b75f52
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-07-09 16:40:41 +0100

    TMP: Add search field

--- Client/src/components/UsersDashboard/UsersDashboard.tsx
+++ Client/src/components/UsersDashboard/UsersDashboard.tsx
@@ -4,6 +4,7 @@ import { LoginResponseData } from '../../shared/types';
 import UserCard from './UserCard';
 
 import useFetchUsers from '@/reactQuery/hooks/useFetchUsers';
+import ActionSearchFiled from '@/UI/formComponents/ActionSearchFiled';
 import ButtonCreateUser from '@/UI/formComponents/ButtonCreateUser';
 import ButtonShowActiveOrInactiveUsers from '@/UI/formComponents/ButtonShowActiveOrInactiveUsers';
 import { getUserData } from '@/util/util';
@@ -11,13 +12,21 @@ import { getUserData } from '@/util/util';
 export default function UsersDashboard() {
     const [active, setActive] = useState<boolean>(true);
     const currentUser: LoginResponseData | undefined = getUserData();
-    const { data: users, isLoading } = useFetchUsers({ status: active ? 'active' : 'inactive' });
+    const {
+        data: users,
+        isLoading,
+        filter,
+        handleChangeFilter,
+    } = useFetchUsers({ status: active ? 'active' : 'inactive' });
 
     return (
         <div className='mx-auto flex flex-col gap-6 p-5'>
             <div className='flex gap-5'>
                 <ButtonShowActiveOrInactiveUsers active={active} setActive={setActive} />
                 {currentUser?.userRole === 'admin' && <ButtonCreateUser />}
+            </div>
+            <div className='flex justify-center'>
+                <ActionSearchFiled value={filter} handleChangeFilter={handleChangeFilter} />
             </div>
             <h2 className='self-center font-bold'>{active ? 'Active' : 'Inactive'} Users</h2>
             {isLoading ? (
