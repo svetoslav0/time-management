6c861c47cf7dc15cc3b3a0ad1a29e915fd35ccbe
AlexPetrov88 <112386979+AlexPetrov88@users.noreply.github.com>
2024-07-27 23:09:19 +0300

    TMP-206

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -54,6 +54,7 @@ function App() {
                         {
                             path: 'users/:id',
                             element: <UserPage />,
+                            loader: restrictAdminPages,
                         },
                         {
                             path: 'projects',
