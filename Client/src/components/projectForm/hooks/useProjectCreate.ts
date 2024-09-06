fee12c9a6bd5efa30cdcc86a4eeb050be1f8c726
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-09-06 16:26:35 +0100

    Change data type

--- Client/src/components/projectForm/hooks/useProjectCreate.ts
+++ Client/src/components/projectForm/hooks/useProjectCreate.ts
@@ -2,18 +2,20 @@ import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
+import { ProjectFormDataType } from '../types';
+
 import { queryKeys, urlKeys } from '@/reactQuery/constants';
 import { queryClient } from '@/reactQuery/queryClient';
 import httpServices from '@/services/httpServices';
-import { ProjectDataType, ProjectResponseDataType } from '@/shared/types';
+import { ProjectResponseDataType } from '@/shared/types';
 
 export default function useProjectCreate() {
     const { post } = httpServices();
     const navigate = useNavigate();
 
     const { mutate: createProject, isSuccess } = useMutation({
-        mutationFn: (data: ProjectDataType) =>
-            post<ProjectDataType, ProjectResponseDataType>(urlKeys.createProject, data),
+        mutationFn: (data: ProjectFormDataType) =>
+            post<ProjectFormDataType, ProjectResponseDataType>(urlKeys.createProject, data),
         onSuccess: (data) => {
             toast.success(`${data.projectName} created successfully`);
             queryClient.invalidateQueries({
