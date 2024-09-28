ed7de0bfeeab229e2993d2127264a56440bb8ee0
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2024-09-28 21:24:24 +0300

    style fix

--- Client/src/UI/design/GoogleSvg.tsx
+++ Client/src/UI/design/GoogleSvg.tsx
@@ -8,6 +8,7 @@ export default function GoogleSvg({ width = 21, height = 21 }: SvgType) {
             viewBox='0 0 21 21'
             fill='none'
             xmlns='http://www.w3.org/2000/svg'
+            className='inline-block'
         >
             <g id='flat-color-icons:google'>
                 <path
