aa6d2228e36f806a3ff0c49c9f220f04007ad342
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-10-03 17:29:48 +0300

    Added util for validating report parameters

---
+++ Server/utils/validationUtils/validateReportParamsUtil.js
@@ -0,0 +1,42 @@
+const Project = require("../../models/Project");
+
+const ProjectValidationErrors = require("../../errors/projectsValidationErrors");
+const { validateObjectId } = require("../validateObjectIdUtil");
+const validateDate = require("../validateDateUtil");
+
+const validateReportParams = async (name, projectId, startDate, endDate) => {
+    if (!name) {
+        throw new ProjectValidationErrors("Name parameter is required!");
+    }
+
+    if (!projectId) {
+        throw new ProjectValidationErrors("ProjectId parameter is required!");
+    }
+
+    if (!validateObjectId(projectId)) {
+        throw new ProjectValidationErrors(`ProjectId parameter is invalid: ${projectId}`);
+    }
+
+    const projectExists = await Project.findById(projectId);
+    if (!projectExists) {
+        throw new ProjectValidationErrors(`ProjectId does not exist: ${projectId}`);
+    }
+
+    if (!startDate) {
+        throw new ProjectValidationErrors("StartDate parameter is required!");
+    }
+    
+    if (await validateDate(startDate)) {
+        throw new ProjectValidationErrors(`StartDate has invalid value: ${startDate}`);
+    }
+
+    if (!endDate) {
+        throw new ProjectValidationErrors("EndDate parameter is required!");
+    }
+
+    if (await validateDate(endDate)) {
+        throw new ProjectValidationErrors(`EndDate has invalid value: ${endDate}`);
+    }
+};
+
+module.exports = validateReportParams;
