dd8e3bdfd8aea408fee3ea9db53bc5cd148b9d89
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-10-07 14:24:57 +0100

    Add invalidate query on success

--- Client/src/components/project/hooks/useGenerateReport.ts
+++ Client/src/components/project/hooks/useGenerateReport.ts
@@ -1,12 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
 
-import { urlKeys } from '@/reactQuery/constants';
+import { queryKeys, urlKeys } from '@/reactQuery/constants';
+import { queryClient } from '@/reactQuery/queryClient';
 import httpServices from '@/services/httpServices';
 
 export default function useGenerateReport() {
     const { post } = httpServices();
-
+    const { id } = useParams<{ id: string }>();
     const {
         mutate: generateReport,
         isSuccess: isGenerateSuccess,
@@ -21,6 +23,9 @@ export default function useGenerateReport() {
         }) => post(urlKeys.report, data),
         onSuccess: () => {
             toast.success('Report was create successfully!');
+            queryClient.invalidateQueries({
+                queryKey: [queryKeys.reports, queryKeys.all, id],
+            });
         },
         onError: (error) =>
             toast.error(error.message, {
